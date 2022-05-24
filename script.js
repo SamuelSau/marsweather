633de91028f85227557b0b5c54bee8aa017265f5
Samuel Sau <samuel51@usf.edu>
2022-05-24 19:33:56 -0400

    created getWeather() to fetch data

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ API_KEY = 'DEMO_KEY';
 
 API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
 
+async function getWeather(){
+    const fetched = await fetch(API_URL)
+    const result = await fetched.json()
+    console.log(result)
+}
+
+getWeather();
